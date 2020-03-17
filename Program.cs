74e107f10ac64a3620be43cb5fbcd7d4cafba459
PietjeGeluk <62058850+PietjeGeluk@users.noreply.github.com>
2020-03-17 12:48:55 +0100

    Add files via upload
    Properties:
    
    class Person
    {
      private string name; // field
      public string Name   // property
      {
        get { return name; }
        set { name = value; }
      }
    }
    
    class Program
    {
      static void Main(string[] args)
      {
        Person myObj = new Person();
        myObj.Name = "Liam";
        Console.WriteLine(myObj.Name);
      }
    }

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person x = new Person();
+            x.Name = "Kees";
+            Console.WriteLine(x.Name);
+        }
+
+
+        class Person
+        {
+            private string name;
+
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+        }
+    }
+}
